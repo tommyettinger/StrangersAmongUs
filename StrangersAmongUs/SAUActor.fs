e9a184b9d4bcb0a704fef5274a97740b265ee269
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-01-08 15:45:53 -0800

    Now the tiles can stack and the units go on top

--- StrangersAmongUs/SAUActor.fs
+++ StrangersAmongUs/SAUActor.fs
@@ -15,15 +15,16 @@ type WorldActor =
     {
         ActorType : ActorType;
         Position : Vector2;
+        GridSpace : Vector3;
         Size : Vector2;
         Texture : Texture2D option;
     }
     member this.CurrentBounds
         with get () = Rectangle((int this.Position.X),(int this.Position.Y),(int this.Size.X),(int this.Size.Y))
 
-let CreateActor (content:ContentManager) (textureName, actorType, position, size) =
+let CreateActor (content:ContentManager) (k, (textureName, actorType, position, gridSpace, size)) =
     let tex = if not (System.String.IsNullOrEmpty textureName) then
                   Some(content.Load textureName)
               else
                   None
-    { ActorType = actorType; Position = position; Size = size; Texture = tex; }
+    { ActorType = actorType; Position = position; GridSpace = gridSpace; Size = size; Texture = tex; }
--- StrangersAmongUs/SAUGame.fs
+++ StrangersAmongUs/SAUGame.fs
@@ -16,19 +16,43 @@ type Game1 () as x =
     let R = new System.Random()
     let RandomTerrain () =  List.nth ["palette50/Terrain_Huge_face0_0"; "palette51/Terrain_Huge_face0_0";] (R.Next 2)
     let RandomCreature () =  List.nth ["palette0/Human_Male_Large_face0_0"; "palette0/Human_Female_Large_face0_0"; "palette20/Lomuk_Large_face0_0";] (R.Next 3)
-    let WorldObjects = lazy ([for x in 0.f .. mapSize do
+    let WorldLogical = [for x in 0.f .. mapSize do
                                   for y in 0.f .. mapSize do
-                                      yield (RandomTerrain(), Terrain, Vector2(48.f * (x - y), 24.f * (x + y)), Vector2(168.f,208.f));]
-                         |> List.sortBy (fun (_, _, v, _) -> v.Y + 0.0001f * v.X)
-                         |> List.map CreateActor')
-
-    let MobileObjects = lazy ([for x in 0.f .. mapSize do
+                                      for r in 0 .. R.Next(4) do
+                                          yield (RandomTerrain(), Terrain, Vector2(48.f * (x - y), 24.f * (x + y) - 18.f * (float32 r)), Vector3(Vector2(x,y), (float32 r)), Vector2(168.f,208.f));]
+                         |> List.map (fun (a, b, v, cv, e) -> ((cv.X, cv.Y, cv.Z), (a, b, v, cv, e)))
+                         |> Map.ofList
+    let HeightMap =  [for x in 0.f .. mapSize do
+                          for y in 0.f .. mapSize do
+                               if(WorldLogical.ContainsKey(x, y, 3.f)) then yield ((x, y), 3.f) else
+                                 if(WorldLogical.ContainsKey(x, y, 2.f)) then yield ((x, y), 2.f) else
+                                   if(WorldLogical.ContainsKey(x, y, 1.f)) then yield ((x, y), 1.f) else
+                                     if(WorldLogical.ContainsKey(x, y, 0.f)) then yield ((x, y), 0.f)] |> Map.ofList
+    
+    let WorldObjects = lazy (WorldLogical
+                         |> Map.toList
+                         |> List.sortBy (fun ((x, y, z), (_, _, v, cv, _)) -> cv.Z * 1000.f + cv.Y + 0.0001f * cv.X))
+    
+    let MobileLogical = [for x in 0.f .. mapSize do
                                   for y in 0.f .. mapSize do
                                       if R.Next(4) = 0 then
-                                          yield (RandomCreature(), Player(Nothing), Vector2(40.f + 48.f * (x - y), 60.f + 24.f * (x + y)), Vector2(88.f,108.f));]
-                         |> List.sortBy (fun (_, _, v, _) -> v.Y + 0.0001f * v.X)
-                         |> List.map CreateActor')
-                         
+                                          let z = (x, y) |> HeightMap.TryFind
+                                          if z.IsSome then yield (RandomCreature(),
+                                                                  Player(Nothing),
+                                                                  Vector2(40.f + 48.f * (x - y), 60.f + 24.f * (x + y) - 18.f * z.Value),
+                                                                  Vector3(Vector2(x,y), (float32 z.Value)),
+                                                                  Vector2(88.f,108.f));]
+                         |> List.map (fun (a, b, v, cv, e) -> ((cv.X, cv.Y, cv.Z), (a, b, v, cv, e)))
+                         |> Map.ofList
+    let MobileObjects = lazy(MobileLogical
+                         |> Map.toList
+                         |> List.sortBy (fun ((x, y, z), (_, _, v, cv, _)) -> cv.Z * 1000.f + cv.Y + 0.0001f * cv.X)
+                         )
+
+
+    let AllObjects = lazy(WorldObjects.Value @ MobileObjects.Value
+                             |> List.sortBy (fun ((x, y, z), (_, _, v, cv, _)) -> cv.Z / 4.f + cv.Y + cv.X)
+                             |> List.map CreateActor')
     let mutable font = Unchecked.defaultof<SpriteFont>
     let mutable frameRate = 0.0
     let mutable frameCounter = 0.0
@@ -68,7 +92,7 @@ type Game1 () as x =
         do x.GraphicsDevice.Clear Color.CornflowerBlue
         let DrawActor' = DrawActor spriteBatch
         do spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, camera.TranslationMatrix)
-        WorldObjects.Value @ MobileObjects.Value |> List.iter DrawActor'
-        do spriteBatch.DrawString(font, (string (int frameRate)), (camera.Position), Color.OrangeRed)
+        AllObjects.Value |> List.iter DrawActor'
+        //do spriteBatch.DrawString(font, (string (int frameRate)), (camera.Position), Color.OrangeRed)
         do spriteBatch.End ()
         ()
